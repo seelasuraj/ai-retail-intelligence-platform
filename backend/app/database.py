2f55970faf19e552eb1d4f9389c61bf047df6d1c
suraj <seelasuraj78@gmail.com>
2026-06-05 21:35:23 +0530

    fix database render crash

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,17 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 Base = declarative_base()
 
-load_dotenv()
+# fallback added (VERY IMPORTANT for Render)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 
-engine = create_engine(DATABASE_URL)
-print("DATABASE_URL =", DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
